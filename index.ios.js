f6e54f81f748a1b7424dc673bd1dbe7e9486543c
Marcus Pennington <marcus.pennington@sohonet.com>
2016-09-26 22:02:53 +0100

    Working on ios

--- index.ios.js
+++ index.ios.js
@@ -11,43 +11,25 @@ import {
   Text,
   View
 } from 'react-native';
+import { Container, Header, Title, Content, Button, Icon } from 'native-base';
 
 class AwesomeNativeBase extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
-  }
+    render() {
+        return (
+            <Container>
+                <Header>
+                  <Button transparent>
+                    <Icon name='ios-arrow-back' />
+                  </Button>
+
+                  <Title>My awesome app</Title>
+
+                  <Button transparent>
+                    <Icon name='ios-menu' />
+                  </Button>
+                </Header>
+            </Container>
+        );
+    }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
-
 AppRegistry.registerComponent('AwesomeNativeBase', () => AwesomeNativeBase);
